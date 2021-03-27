8847453c50fbb819db942bd493817da6874b4533
Ruslan Shevcov <sasa02sasa@mail.ru>
2021-03-27 19:35:05 +0600

    Добавлен класс Scan

---
+++ Scan.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    public class Scan
+    {
+        public string ScanName { get; set; }
+        public string Db { get; set; }
+        public string Server { get; set; }
+        public int ErrorCount { get; set; }
+    }
+}
